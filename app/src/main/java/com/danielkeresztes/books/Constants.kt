65d6647be4f6018982a6fd863bc14ea3f331dffe
Daniel <daniel.keresztes@supercharge.io>
2018-07-14 19:14:14 +0200

    Add Constants

---
+++ app/src/main/java/com/danielkeresztes/books/Constants.kt
@@ -0,0 +1,8 @@
+package com.danielkeresztes.books
+
+
+const val API_URL: String = "https://www.googleapis.com/books/v1/"
+const val API_QUERY_PARAM = "q"
+const val API_QUERY_MAXRESULTS = "maxResults"
+const val SEARCH_DEFAULT: String = "software"
+const val SEARCH_MAXRESULTS: String = "40"
